aadb51e0e6250b1b84c7f1c4c9ce2b22bf61633d
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-09-04 14:20:37 +0300

    fix: vercel deployment issue

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import app from "../src/index";
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  await app.ready();
+  app.server.emit("request", req, res);
+}
+
+
