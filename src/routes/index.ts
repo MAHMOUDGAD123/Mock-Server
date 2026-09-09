eb21b9205c2c46b2f319d0efc66701a7442f01bd
Mahmoud_Gad <74635547+MAHMOUDGAD123@users.noreply.github.com>
2026-09-09 07:58:50 +0300

    make sime updates

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance } from "fastify";
 import { usersRoutes } from "@/routes/users";
 import { postsRoutes } from "@/routes/posts";
-import { youtubeRoutes } from "@/routes/youtubei";
+// import { youtubeRoutes } from "@/routes/youtubei";
 import { shopRoutes } from "@/routes/shop";
 import { cacheRoutes } from "@/routes/cache";
 
@@ -13,6 +13,6 @@ export const appRoutes = async (app: FastifyInstance) => {
   app.register(shopRoutes, { prefix: "/shop" });
   app.register(usersRoutes, { prefix: "/users" });
   app.register(postsRoutes, { prefix: "/posts" });
-  app.register(youtubeRoutes, { prefix: "/youtube" });
+  // app.register(youtubeRoutes, { prefix: "/youtube" });
   app.register(cacheRoutes, { prefix: "/cache" });
 };
